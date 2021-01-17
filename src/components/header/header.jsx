5f0b55af3fcceb010f5911a02b4964ab581bfb9d
abdulhaseeb036 <alamhaseeb036@gmail.com>
2021-01-18 02:53:32 +0500

    changes header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ import './header.css'
 
 function Header() {
     return (
-      <h1>Expence Tracker App By <br/> <a className="my-link" rel='noopener' href="https://haseebalamrafiq.web.app" target="_blank">Haseeb Alam Rafiq</a> <hr className="header-divider" /></h1>
+      <h1>Expence Tracker App By <br/> <a className="my-link" href="https://haseebalamrafiq.web.app">Haseeb Alam Rafiq</a> <hr className="header-divider" /></h1>
     )
 }
 export default Header;
